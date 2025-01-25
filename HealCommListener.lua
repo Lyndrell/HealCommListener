aa4b545fad9291b295c600ba3e3a7d2d9f9b45d6
Your Name <you@example.com>
2025-01-25 22:43:41 +0100

    first version without checking who is NOT using HealComm

---
+++ HealCommListener.lua
@@ -0,0 +1,35 @@
+local HealCommUsers = {}
+
+SLASH_HEALCOMMLISTENER1 = "/hcl"
+SLASH_HEALCOMMLISTENER2 = "/HCL"
+SlashCmdList["HEALCOMMLISTENER"] = function(msg)
+	print("HealCommListener: Players need to cast spells while in your group in order to appear in the list. /hclreset for flushing the list")
+	print("/hclreset for flushing the list.")
+	print("/hclsave for saving the list to a file.")
+	print("Listing players in the HealComm user list:")
+	for i,_ in pairs(HealCommUsers) do print(i) end
+end
+
+SLASH_HEALCOMMLISTENERRESET1 = "/hclreset"
+SLASH_HEALCOMMLISTENERRESET2 = "/HCLRESET"
+SlashCmdList["HEALCOMMLISTENERRESET"] = function(msg)
+	print("HealCommListener: Players in the list have been flushed. You can also /reload to do this.")
+	HealCommUsers = {}
+end
+
+SLASH_HEALCOMMLISTENERSAVE1 = "/hclsave"
+SLASH_HEALCOMMLISTENERSAVE2 = "/HCLSAVE"
+SlashCmdList["HEALCOMMLISTENERSAVE"] = function(msg)
+	print("HealCommListener: Players in the list will saved to HealCommListener.lua in your WTF Account and character variables on logout.")
+	HCUSERLIST = HealCommUsers
+end
+
+local HealCommListener = CreateFrame("Frame")
+HealCommListener:RegisterEvent("CHAT_MSG_ADDON")
+HealCommListener:SetScript("OnEvent", function()
+	if arg1 == "HealComm" then
+		if HealCommUsers[arg4] == nil then
+			HealCommUsers[arg4] = true
+		end	
+	end
+end)
